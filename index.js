b57fdf95167e2ec11e00203acf2f17962cfd8ca5
uhomuho <uhomuho@gmail.com>
2021-03-26 15:36:36 +0500

    Working release

--- index.js
+++ index.js
@@ -17,5 +17,28 @@ app.get('/', (req, res) => {
 	res.send('ROY Club API for Google Spreadsheets')
 })
 
+ 
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+app.post('/add', async (req, res) => {
+	try {
+		const { id, data } = req.body,
+					doc = new GoogleSpreadsheet(id)
+
+		await doc.useServiceAccountAuth({
+			client_email: process.env.EMAIL,
+			private_key: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
+		})
+		await doc.loadInfo()
+	
+		const sheet = doc.sheetsByIndex[0]
+		await sheet.addRow(data)
+
+		res.sendStatus(200)
+	} catch (err) {
+		console.error(err)
+		res.sendStatus(500)
+	}
+})
+
 let PORT = process.env.PORT || 8082
 const server = app.listen( PORT, () => console.log(`Server running on port ${PORT}...`) )
